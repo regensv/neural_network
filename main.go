da7351a2dd5158e7766bdd83a81a1f68cde86e29
regensv <regensv@gmail.com>
2019-06-25 15:32:55 +0300

    perceptron

--- main.go
+++ main.go
@@ -3,130 +3,79 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
+	"time"
 )
 
-func sigmoid(v float64) float64 {
-	return 1 / (1 + math.Pow(math.E, -v))
-}
-
-func activation(v float64) float64 {
-	return sigmoid(v)
-}
-
 type Neuron struct {
-	w     float64
-	val   float64
-	prev  []*Neuron
-	delta float64
-	wGrad float64
+	weights []float64
 }
 
-func (n *Neuron) getValue() float64 {
-	if len(n.prev) != 0 {
-		var val float64 = 0
-		for _, in := range n.prev {
-			val += in.getValue() * in.w
+func (n *Neuron) init(inputsCount int) {
+	rand.Seed(time.Now().Unix())
+	n.weights = make([]float64, inputsCount)
+	for i, _ := range n.weights {
+		n.weights[i] = rand.Float64()*2 - 1
+	}
+}
+
+func (n *Neuron) think(inputs []float64) float64 {
+	var sum float64 = 0
+	for i, w := range n.weights {
+		sum += w * inputs[i]
+	}
+	return sigmoid(sum)
+}
+
+func (n *Neuron) train(trainingSetInputs [][]float64, trainingSetOutputs []float64, iterations int) {
+	for i := 0; i < iterations; i++ {
+		adjustments := make([]float64, len(n.weights))
+
+		for setIdx, inputs := range trainingSetInputs {
+			output := n.think(inputs)
+			err := trainingSetOutputs[setIdx] - output
+
+			for col := 0; col < len(n.weights); col++ {
+				adjustments[col] += trainingSetInputs[setIdx][col] * err * sigmoidDerivative(output)
+			}
 		}
-		n.val = activation(val)
-	}
 
-	// fmt.Printf("weight: %f, value: %f\n", n.w, n.val)
-	return n.val
-}
-
-func (n *Neuron) UpdateW(delta float64) {
-	var E float64 = 0.7 // learn speed
-	var A float64 = 0.1 // moment
-
-	deltaNew := ((1 - n.val) * n.val) * (n.w * delta)
-	n.wGrad = n.val * delta
-	deltaW := E*n.wGrad + A*n.delta
-	n.delta = deltaNew
-	n.w += deltaW
-
-	for _, n := range n.prev {
-		n.UpdateW(n.delta)
-	}
-}
-
-func think(nIn1 *Neuron, nIn2 *Neuron, nOut *Neuron, in1 float64, in2 float64) float64 {
-	nIn1.val = in1
-	nIn2.val = in2
-	result := nOut.getValue()
-	return result
-}
-
-func train(nIn1 *Neuron, nIn2 *Neuron, nOut *Neuron, sets [][]float64) {
-	var setsNum int = 0
-	var errSum float64 = 0
-
-	for i := 0; i < len(sets); i++ {
-		result := think(nIn1, nIn2, nOut, sets[i][0], sets[i][1])
-		// fmt.Printf("set %d. Result: %f\n", i, result)
-
-		diff := sets[i][2] - result
-		fmt.Printf("result: %f, diff: %f\n", result, diff)
-		errSum += diff * diff
-		setsNum++
-
-		nOut.delta = diff * ((1 - result) * result)
-		for _, n := range nOut.prev {
-			n.UpdateW(nOut.delta)
+		for i := 0; i < len(n.weights); i++ {
+			n.weights[i] += adjustments[i]
 		}
 	}
+}
 
-	err := errSum / float64(setsNum)
+func sigmoid(x float64) float64 {
+	return 1 / (1 + math.Pow(math.E, -x))
+}
 
-	fmt.Printf("Epoch error: %f\n", err)
+func sigmoidDerivative(x float64) float64 {
+	return x * (1 - x)
 }
 
 func main() {
-	nI1 := Neuron{0.45, 0.5, []*Neuron{}, 0.0, 0.0}
-	nI2 := Neuron{-0.12, 0.4, []*Neuron{}, 0.0, 0.0}
+	var n Neuron
+	n.init(3)
+	fmt.Printf("Start Weights: %v\n", n.weights)
 
-	nH1 := Neuron{1.5, 0.0, []*Neuron{
-		&nI1,
-		&nI2,
-	}, 0.0, 0.0}
-
-	nO1 := Neuron{0.0, 0.0, []*Neuron{
-		&nH1,
-	}, 0.0, 0.0}
-
-	var iterations int = 10
-
-	trainingData := [][]float64{
-		[]float64{0.5, 0.4, 0.9},
-		[]float64{0.1, 0.1, 0.2},
-		[]float64{0.3, 0.1, 0.4},
-		[]float64{0.7, 0.05, 0.75},
+	trainingSetInputs := [][]float64{
+		{0, 0, 0},
+		{0, 0, 1},
+		{0, 1, 0},
+		{0, 1, 1},
+		{1, 0, 0},
+		{1, 0, 1},
+		{1, 1, 0},
+		{1, 1, 1},
 	}
+	trainingSetOutputs := []float64{0, 0, 0, 0, 1, 1, 1, 1}
 
-	for i := 0; i < iterations; i++ {
-		train(&nI1, &nI2, &nO1, trainingData)
-	}
+	test := []float64{0, 1, 1}
+	var res float64
 
-	result := think(&nI1, &nI2, &nO1, 0.1, 0.1)
-	fmt.Printf("result: %f\n", result)
-
-	// var guess float64
-	// guess = n.think(0.5, 0.4)
-	// fmt.Printf("Guessed: %f\n", guess)
-	// guess = n.think(0.1, 0.3)
-	// fmt.Printf("Guessed: %f\n", guess)
-	// guess = n.think(0.6, 0.2)
-	// fmt.Printf("Guessed: %f\n", guess)
-
-	// 	fmt.Printf("weights: (%f, %f)\n", nn.w1, nn.w2)
-
-	// 	guess := nn.think(2, 3)
-	// 	fmt.Printf("Guessed: %f\n", guess)
-	// 	guess = nn.think(5, 4)
-	// 	fmt.Printf("Guessed: %f\n", guess)
-	// 	guess = nn.think(0, 0)
-	// 	fmt.Printf("Guessed: %f\n", guess)
-	// 	guess = nn.think(12, 3)
-	// 	fmt.Printf("Guessed: %f\n", guess)
-	// 	guess = nn.think(32, 17)
-	// 	fmt.Printf("Guessed: %f\n", guess)
+	n.train(trainingSetInputs, trainingSetOutputs, 50000)
+	fmt.Printf("Weights: %v\n", n.weights)
+	res = n.think(test)
+	fmt.Printf("Result: %f\n", res)
 }
